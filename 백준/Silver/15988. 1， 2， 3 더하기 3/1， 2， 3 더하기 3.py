de0c09b246096b4ec26ce27e50fde1bc4dd8e293
sxonz <taebal2020@gmail.com>
2025-04-30 18:12:56 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 148 ms, Memory: 154812 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -0,0 +1,8 @@
+testcase = int(input())
+d = [int(input()) for _ in range(testcase)]
+n = max(d)
+dp = [1,1,2]
+for i in range(3,n+1):
+    dp += [(dp[i-1] + dp[i-2] + dp[i-3]) % 1000000009]
+for i in d:
+    print(dp[i])
