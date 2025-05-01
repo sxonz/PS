6c700912cd50f5354a83d861ca0d17812853c92b
sxonz <taebal2020@gmail.com>
2025-05-01 23:17:02 +0900

    [Gold III] Title: MST 게임, Time: 280 ms, Memory: 114604 KB -BaekjoonHub

---
+++ 백준/Gold/16202. MST 게임/MST 게임.py
@@ -0,0 +1,28 @@
+ans = []
+n,m,k = map(int,input().split())
+edges = []
+for i in range(m):
+    a,b = map(int,input().split())
+    edges += [(i+1,a,b)]
+for turn in range(k):
+    p = list(range(n+1))
+    def F(x):
+        if x != p[x]:p[x] = F(p[x])
+        return p[x]
+    def U(a,b):
+        a,b = F(a),F(b)
+        if a<b:
+            p[b] = a
+        else:
+            p[a] = b
+    cost = 0
+    for c,a,b in edges:
+        if F(a) != F(b):
+            U(a,b)
+            cost += c
+    if len(set([F(i) for i in range(1,n+1)])) != 1:
+        cost = 0
+    if edges:
+        edges.pop(0)
+    ans.append(cost)
+print(*ans)
