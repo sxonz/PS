ceb78b1b7c91765a57192c8eed9b2825e446425a
sxonz <taebal2020@gmail.com>
2025-05-10 17:39:46 +0900

    [Platinum IV] Title: 혹 떼러 갔다 혹 붙여 온다, Time: 1732 ms, Memory: 249960 KB -BaekjoonHub

--- 백준/Platinum/20931. 혹 떼러 갔다 혹 붙여 온다/혹 떼러 갔다 혹 붙여 온다.py
+++ 백준/Platinum/20931. 혹 떼러 갔다 혹 붙여 온다/혹 떼러 갔다 혹 붙여 온다.py
@@ -1,33 +1,17 @@
 import sys
-input = sys.stdin.readline
-log = 19
-Q = int(input())
-parents = [[0]*log]
-distance = [[int(2e22)]*log]
-m = 1
+I,R=sys.stdin.readline,range
+L,M=19,-1
+P,D,m,last=[[0]*L],[[int(2e22)]*L],1,0
 def adhoc(k,l):
-    global m
-    x = (k+last) % m
-    tmp = [x]
-    dtmp = [l]
-    for r in range(1,log):
-        tmp.append(parents[tmp[-1]][r-1])
-        dtmp.append(dtmp[-1] + distance[tmp[r-1]][r-1])
-    parents.append(tmp)
-    distance.append(dtmp)
-    m += 1
+    global m;x,y=[(k+last)%m],[l]
+    for r in R(L-1):x+=[P[t:=x[M]][r]];y+=[y[M] + D[t][r]]
+    P.append(x);D.append(y);m+=1
 def query(x,l):
     global last
-    for k in range(log-1,-1,-1):
-        if l >= distance[x][k]:
-            l -= distance[x][k]
-            x = parents[x][k]
-    last = x
-    print(x)
-last = 0
-for q in range(Q):
-    s = input().split()
-    if s[0] == "query":
-        query(int(s[1]),int(s[2]))
-    else:
-        adhoc(int(s[1]),int(s[2]))
+    for k in R(L-1,M,M):
+        if l>=(d:=D[x][k]):l-=d;x=P[x][k]
+    print(last:=x)
+for q in R(int(I())):
+    s,a,b = I().split();a,b=int(a),int(b)
+    if"q"==s[0]:query(a,b)
+    else:adhoc(a,b)
