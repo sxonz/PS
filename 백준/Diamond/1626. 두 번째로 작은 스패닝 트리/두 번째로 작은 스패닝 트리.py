23c2d2c7a188b307ffd5f4b60cf98efcad460669
sxonz <taebal2020@gmail.com>
2025-05-21 09:45:54 +0900

    [Diamond IV] Title: 두 번째로 작은 스패닝 트리, Time: 1636 ms, Memory: 281116 KB -BaekjoonHub

--- 백준/Diamond/1626. 두 번째로 작은 스패닝 트리/두 번째로 작은 스패닝 트리.py
+++ 백준/Diamond/1626. 두 번째로 작은 스패닝 트리/두 번째로 작은 스패닝 트리.py
@@ -1,17 +1,15 @@
 import sys
-input = sys.stdin.readline
+I,R = lambda:map(int,sys.stdin.readline().split()),range
 sys.setrecursionlimit(60000)
 
 log = 19
-n,m = map(int,input().split())
-edges = []
-d = [[] for i in range(n+1)]
-for i in range(m):
-    a,b,c = map(int,input().split())
-    edges.append((c,a,b))
-edges.sort()
+n,m = I()
+r,L = R(n+1), R(18,-1,-1)
+edges = [list(I()) for i in range(m)]
+edges.sort(key=lambda x:x[2])
+d = [[] for i in r]
 
-parents = list(range(n+1))
+parents = list(r)
 def F(x):
     if x != parents[x]: parents[x] = F(parents[x])
     return parents[x]
@@ -21,85 +19,69 @@ def U(a,b):
         parents[b] = a
     else:
         parents[a] = b
-
+def M(l):
+    l = list(set(l))
+    l.sort()
+    return (l[-1],l[-2])
 cost = 0
-for c,a,b in edges:
+for a,b,c in edges:
     if F(a) != F(b):
         U(a,b)
         d[a].append((b,c))
         d[b].append((a,c))
         cost += c
-if len(set([F(i) for i in range(1,n+1)])) != 1:
+if len(set([F(i) for i in r[1:]])) != 1:
     print(-1)
-else:
-    p = [[0]*log for i in range(n+1)]
-    mdist = [[(-1,-1) for j in range(log)] for i in range(n+1)]
-    depth = [0]*(n+1)
-    visited = [0]*(n+1)
-    depth[1] = 1
-    visited[1] = 1
+    exit(0)
 
-    def dfs(x):
-        for nx,c in d[x]:
-            if not visited[nx]:
-                visited[nx] = 1
-                depth[nx] = depth[x] + 1
-                p[nx][0] = x
-                mdist[nx][0] = (c,-1)
-                dfs(nx)
-    dfs(1)
+p = [[0]*log for i in r]
+mdist = [[(-1,-1) for j in R(log)] for i in r]
+depth = [1]+[0]*n
+def dfs(x,t):
+    for nx,c in d[x]:
+        if not depth[nx]:
+            depth[nx] = t
+            p[nx][0] = x
+            mdist[nx][0] = (c,-1)
+            dfs(nx,t+1)
+dfs(1,2)
 
-    for k in range(1,log):
-        for i in range(1,n+1):
-            tmp = mdist[i][k-1]
-            tmp2 = mdist[p[i][k-1]][k-1]
-            dists = list(set([tmp[0],tmp[1],tmp2[0],tmp2[1],-1,-2]))
-            dists.sort()
-            mdist[i][k] = (dists[-1],dists[-2])
-            p[i][k] = p[p[i][k-1]][k-1]
+for k in R(1,log):
+    for i in R(1,n+1):
+        z=k-1
+        x=p[i][z]
+        mdist[i][k] = M([*mdist[i][z],*mdist[x][z],-1,-2])
+        p[i][k] = p[x][z]
 
-    second = float('inf')
+second = float('inf')
 
-    def fs(a,b):
-        f,s = -1,-1
-        if depth[a] < depth[b]:
-            a,b = b,a
-        for k in range(log-1,-1,-1):
-            if depth[a] - depth[b] >= 1<<k:
-                tmp = mdist[a][k]
-                r = list(set([f,s,tmp[0],tmp[1],-1]))
-                r.sort()
-                f,s = r[-1],r[-2]
-                a = p[a][k]
-        if a == b:
-            return (f,s)
+def fs(a,b):
+    f,s = -1,-1
+    if depth[a] < depth[b]:
+        a,b = b,a
+    for k in L:
+        if depth[a] - depth[b] >= 1<<k:
+            f,s = M([f,s,*mdist[a][k],-1])
+            a = p[a][k]
+    if a == b:
+        return (f,s)
 
-        for k in range(log-1,-1,-1):
-            if p[a][k] != p[b][k]:
-                tmp = mdist[a][k]
-                tmp2 = mdist[b][k]
-                r = list(set([f,s,tmp[0],tmp[1],tmp2[0],tmp2[1],-1]))
-                r.sort()
-                f,s = r[-1],r[-2]
-                a = p[a][k]
-                b = p[b][k]
+    for k in L:
+        if p[a][k] != p[b][k]:
+            f,s = M([f,s,*mdist[a][k],*mdist[b][k],-1])
+            a,b= p[a][k],p[b][k]
 
-        r = list(set([f,s,mdist[a][0][0],mdist[b][0][0],-1]))
-        r.sort()
-        return (r[-1],r[-2])
+    return M([f,s,mdist[a][0][0],mdist[b][0][0],-1])
 
-    for c,a,b in edges:
-        dists = fs(a,b)
-        if dists[0] < 0:
-            continue
-        if dists[0] == c:
-            if dists[1] >= 0:
-                second = min(second, cost - dists[1] + c)
-        else:
-            second = min(second, cost - dists[0] + c)
-
-    if second == float('inf'):
-        print(-1)
+for a,b,c in edges:
+    f,s = fs(a,b)
+    if f < 0:
+        continue
+    if f == c:
+        if s >= 0:
+            second = min(second, cost - s + c)
     else:
-        print(second)
+        second = min(second, cost - f + c)
+
+print(-1 if second == float('inf') else second)
 
