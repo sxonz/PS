05a4db632120f4af9e83097f660b1eafef926d73
sxonz <taebal2020@gmail.com>
2025-07-23 15:34:49 +0900

    [Gold V] Title: 퇴사 2, Time: 416 ms, Memory: 122760 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0]*(n+1)
+m = 0
+for i in range(n):
+    dp[i] = max(dp[i],m)
+    m = max(m,dp[i])
+    t,v = map(int,input().split())
+    if i+t > n:
+        continue
+    dp[i+t] = max(dp[i+t],dp[i]+v)
+print(max(dp))
