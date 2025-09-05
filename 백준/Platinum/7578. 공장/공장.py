b816eabd59aeec81347f0c21ae5ceb33fd6e0389
sxonz <taebal2020@gmail.com>
2025-09-05 14:41:52 +0900

    [Platinum V] Title: 공장, Time: 528 ms, Memory: 255720 KB -BaekjoonHub

---
+++ 백준/Platinum/7578. 공장/공장.py
@@ -0,0 +1,28 @@
+n = int(input())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
+idx = {j:i for i,j in enumerate(a)}
+d = []
+for i in b:
+    d.append(idx[i]+1)
+
+bstree = [0]*(n+1)
+
+def search(x):
+    res = 0
+    while x:
+        res += bstree[x]
+        x -= x&-x
+    return res
+
+def update(x):
+    while x <= n:
+        bstree[x] += 1
+        x += x&-x
+
+r = 0
+for i in range(n):
+    r += i-search(d[i])
+    update(d[i])
+print(r)
