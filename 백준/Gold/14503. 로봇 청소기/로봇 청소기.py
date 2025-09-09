3ac0070d040323547f7aa4b42bd9cb2881691498
sxonz <taebal2020@gmail.com>
2025-09-09 21:17:03 +0900

    [Gold V] Title: 로봇 청소기, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,25 +1,20 @@
 dx = (-1,0,1,0)
 dy = (0,1,0,-1)
+I = lambda:map(int,input().split())
+n,m = I()
+r,c,dir = I()
 
-n,m = map(int,input().split())
-r,c,dir = map(int,input().split())
-
-d = [list(map(int,input().split())) for i in range(n)]
-cleaned = [[False]*m for i in range(n)]
-for i in range(n):
-    for j in range(m):
-        if d[i][j]:
-            cleaned[i][j] = True
-
+d = [list(I()) for i in range(n)]
+cleaned = [i[::] for i in d]
 cl = 0
 while 1:
     if not cleaned[r][c]:
         cleaned[r][c] = True
         cl += 1
     if 0 not in[cleaned[r+1][c],cleaned[r][c+1],cleaned[r-1][c],cleaned[r][c-1]]:
-        if not d[r+dx[(dir+2)%4]][c+dy[(dir+2)%4]]:
-            r += dx[(dir+2)%4]
-            c += dy[(dir+2)%4]
+        if not d[r+dx[x:=(dir+2)%4]][c+dy[x]]:
+            r += dx[x]
+            c += dy[x]
             continue
         print(cl)
         break
