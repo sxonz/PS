b4627718ce1e7a11dc3fd15574074531d71cee76
sxonz <taebal2020@gmail.com>
2025-09-17 13:38:57 +0900

    [Platinum IV] Title: 영화 수집, Time: 1908 ms, Memory: 71500 KB -BaekjoonHub

--- 백준/Platinum/3653. 영화 수집/영화 수집.py
+++ 백준/Platinum/3653. 영화 수집/영화 수집.py
@@ -6,17 +6,17 @@ for test in range(testcase):
     n,m = map(int,input().split())
     query = list(map(int,input().split()))
     idx = {i:n-i+1 for i in range(1,n+1)}
-
+    
     size = n+m+1
     cur = n+1
     bstree = [0]*size
-
+    
     def build():
         for i in range(1,n+1):
             while i<size:
                 bstree[i] += 1
                 i += i&-i
-
+                
     def search(num):
         x = idx[num]
         res = n
@@ -24,10 +24,12 @@ for test in range(testcase):
             res -= bstree[x]
             x -= x&-x
         return res
+    
     def update(pos):
         while pos < size:
             bstree[pos] += 1
             pos += pos&-pos
+    
     def remove(num):
         x = idx[num]
         while x < size:
