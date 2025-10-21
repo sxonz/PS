0ebc59c0e03d1dd6a1ee51b2467629f54252690a
sxonz <taebal2020@gmail.com>
2025-10-21 15:20:07 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -0,0 +1,4 @@
+d = [[11,"ABCDEFGHJLM"],[9,"ACEFGHILM"],[9,"ACEFGHILM"],[9,"ABCEFGHLM"],[8,"ACEFGHLM"],[8,"ACEFGHLM"],[8,"ACEFGHLM"],[8,"ACEFGHLM"],[8,"ACEFGHLM"],[8,"ABCFGHLM"]]
+n = int(input())-1
+print(d[n][0])
+print(*d[n][1])
