81194d7bccb68c24dfd34f69a1876398b4fd8d5c
sxonz <taebal2020@gmail.com>
2025-11-03 16:12:53 +0900

    [Gold V] Title: 닭강정의 전설, Time: 2228 ms, Memory: 325588 KB -BaekjoonHub

--- 백준/Gold/30972. 닭강정의 전설/닭강정의 전설.py
+++ 백준/Gold/30972. 닭강정의 전설/닭강정의 전설.py
@@ -2,14 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-board = [list(map(int,input().split())) for i in range(n)]
+psum = [[0]+list(map(int,input().split())) for i in range(n)]
+psum = [[0]*(n+1)] + psum
 
-psum = [[0]*(n+1) for i in range(n+1)]
 for i in range(n):
     for j in range(n):
-        psum[i+1][j+1] = psum[i][j+1] + psum[i+1][j] - psum[i][j] + board[i][j]
+        psum[i+1][j+1] += psum[i][j+1] + psum[i+1][j] - psum[i][j]
 
 for q in range(int(input())):
     a,b,c,d = map(int,input().split())
-    tmp = -psum[c][d] + psum[a-1][d] + psum[c][b-1] - psum[a-1][b-1] + 2*(psum[c-1][d-1] - psum[a][d-1] - psum[c-1][b] + psum[a][b])
-    print(tmp)
+    print(-psum[c][d] + psum[a-1][d] + psum[c][b-1] - psum[a-1][b-1] + 2*(psum[c-1][d-1] - psum[a][d-1] - psum[c-1][b] + psum[a][b]))
