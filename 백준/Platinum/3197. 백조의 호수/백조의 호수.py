a0011a3b4dc824e58409b31e71abbdfb01e738e1
sxonz <taebal2020@gmail.com>
2025-11-11 23:17:07 +0900

    [Platinum V] Title: 백조의 호수, Time: 3296 ms, Memory: 541668 KB -BaekjoonHub

---
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.py
@@ -0,0 +1,86 @@
+import sys
+sys.setrecursionlimit(100000)
+
+dx = (-1,1,0,0)
+dy = (0,0,-1,1)
+
+n,m = map(int,input().split())
+d = [list(input()) for i in range(n)]
+group = [[0]*m for i in range(n)]
+visited = [[False]*m for i in range(n)]
+g = 1
+queue = []
+def dfs1(x,y):
+    for i in range(4):
+        nx = x+dx[i]
+        ny = y+dy[i]
+        if 0<=nx<n and 0<=ny<m and not visited[nx][ny] and d[nx][ny] == ".":
+            visited[nx][ny] = True
+            group[nx][ny] = g
+            queue.append((nx,ny))
+            dfs1(nx,ny)
+
+s = []
+for i in range(n):
+    for j in range(m):
+        if d[i][j] == "L":
+            d[i][j] = "."
+            s.append((i,j))
+s1,s2 = s
+
+for i in range(n):
+    for j in range(m):
+        if not visited[i][j] and d[i][j] == ".":
+            visited[i][j] = True
+            queue.append((i,j))
+            group[i][j] = g
+            dfs1(i,j)
+            g += 1
+
+tmp = []
+parents = list(range(g))
+day = 0
+def find(x):
+    if parents[x]-x:
+        parents[x] = find(parents[x])
+    return parents[x]
+def union(a,b):
+    a,b = find(a),find(b)
+    if a<b:
+        parents[b] = a
+    else:
+        parents[a] = b
+
+def dfs2(x,y):
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if 0<=nx<n and 0<=ny<m:
+                if d[nx][ny] == '.':
+                    if not visited[nx][ny]:    
+                        visited[nx][ny] = True
+                        tmp.append((nx,ny))
+                        dfs2(nx,ny)
+                        group[nx][ny] = group[x][y]
+                    else:
+                        union(group[x][y],group[nx][ny])
+while find(group[s1[0]][s1[1]]) != find(group[s2[0]][s2[1]]):
+
+    for x,y in queue:
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if 0<=nx<n and 0<=ny<m:
+                if not visited[nx][ny]:
+                    d[nx][ny] = '.'
+                    visited[nx][ny] = True
+                    tmp.append((nx,ny))
+                    group[nx][ny] = group[x][y]
+                    dfs2(nx,ny)
+                else:
+                    union(group[x][y],group[nx][ny])
+
+    queue = tmp[::]
+    tmp.clear()
+    day += 1
+print(day)
