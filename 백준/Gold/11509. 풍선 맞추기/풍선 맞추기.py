8571e99b0c36b9692dd56ca0820c44fb88e6eb20
sxonz <taebal2020@gmail.com>
2025-11-19 15:09:32 +0900

    [Gold V] Title: 풍선 맞추기, Time: 260 ms, Memory: 216756 KB -BaekjoonHub

--- 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.py
+++ 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.py
@@ -1,9 +1,10 @@
 n = int(input())
 d = list(map(int,input().split()))
-cur = [0]*(max(d)+2)
-ans = 0
+cur = [0]*1000002
+ans = n
 for i in d:
     if cur[i+1]:
         cur[i+1] -= 1
+        ans -= 1
     cur[i] += 1
-print(sum(cur))
+print(ans)
