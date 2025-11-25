ee6a75791db26b24a0587b9e2d5f1ce9d5a4afde
sxonz <taebal2020@gmail.com>
2025-11-25 09:50:38 +0900

    [Platinum II] Title: 최소 버텍스 커버, Time: 720 ms, Memory: 128776 KB -BaekjoonHub

---
+++ 백준/Platinum/2051. 최소 버텍스 커버/최소 버텍스 커버.py
@@ -0,0 +1,52 @@
+from collections import deque
+
+n,m = map(int,input().split())
+d = [[]]
+for i in range(n):
+    t,*r = map(int,input().split())
+    d.append(r)
+
+
+connected = [0]*(m+1)
+def dfs(x):
+    if visited[x]:
+        return False
+    visited[x] = True
+    for nx in d[x]:
+        if not connected[nx] or dfs(connected[nx]):
+            connected[nx] = x
+            return True
+    
+    return False
+
+ans = 0
+for i in range(1,n+1):
+    visited = [False]*(n+1)
+    ans += dfs(i)
+
+a = set(range(1,n+1)) - set(connected)
+
+ax = set()
+bx = set()
+queue = deque([[i,1] for i in a])
+visa = [False]*(n+1)
+visb = [False]*(m+1)
+while queue:
+    x,cur = queue.popleft()
+    if cur:
+        ax.add(x)
+        for nx in d[x]:
+            if not visb[nx]:
+                visb[nx] = True
+                queue.append((nx,0))
+    else:
+        bx.add(x)
+        if not visa[connected[x]]:
+            visa[connected[x]] = True
+            queue.append((connected[x],1))
+
+ra = set([connected[i] for i in range(1,m+1) if connected[i]])-ax
+rb = set([i for i in range(1,m+1) if connected[i]])&bx
+print(ans)
+print(len(ra),*ra)
+print(len(rb),*rb)
