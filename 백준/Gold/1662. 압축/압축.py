d7ba0f53a7bb2a266804751424c236b4b3fbdcee
sxonz <taebal2020@gmail.com>
2025-12-02 08:54:29 +0900

    [Gold IV] Title: 압축, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1662. 압축/압축.py
+++ 백준/Gold/1662. 압축/압축.py
@@ -5,18 +5,18 @@ o = 0
 
 d = [[0,0] for i in range(50)]
 for i in s:
-    if i.isdigit():
+    if i == ")":
+        d[o] = [0,d[o][0] + d[o][1]]
+        o += 1
+    elif i == "(":
+        d[o-1][0] += d[o][0] + d[o][1]
+        d[o] = [0,0]
+        o -= 1
+        flag = True
+    else:
         if flag:
             d[o][0] *= int(i)
             flag = False
         else:
             d[o][0] += 1
-    elif i == ")":
-        d[o] = [0,sum(d[o])]
-        o += 1
-    else:
-        d[o-1][0] += sum(d[o])
-        d[o] = [0,0]
-        o -= 1
-        flag = True
-print(sum(d[0]))
+print(d[0][0] + d[0][1])
