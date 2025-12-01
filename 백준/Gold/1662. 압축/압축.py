4a6abc40f10f0f5d403a66d1ede2962e74a687b7
sxonz <taebal2020@gmail.com>
2025-12-02 08:51:54 +0900

    [Gold IV] Title: 압축, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1662. 압축/압축.py
+++ 백준/Gold/1662. 압축/압축.py
@@ -1,25 +1,22 @@
 s = input()[::-1]
 
-r = 0
 flag = False
 o = 0
 
-stack = [[0,0]]
+d = [[0,0] for i in range(50)]
 for i in s:
     if i.isdigit():
         if flag:
-            stack[-1][0] *= int(i)
+            d[o][0] *= int(i)
             flag = False
         else:
-            stack[-1][0] += 1
+            d[o][0] += 1
     elif i == ")":
-        stack[-1] = [0,sum(stack[-1])]
+        d[o] = [0,sum(d[o])]
         o += 1
-        if len(stack) <= o:
-            stack.append([0,0])
     else:
-        stack[o-1][0] += sum(stack[o])
-        stack.pop()
+        d[o-1][0] += sum(d[o])
+        d[o] = [0,0]
         o -= 1
         flag = True
-print(sum(stack[0]))
+print(sum(d[0]))
