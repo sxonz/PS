ef5191ea36816dbff5e5ca882b7c0b90ecd76490
sxonz <taebal2020@gmail.com>
2025-12-10 15:15:19 +0900

    [Gold II] Title: 수열의 점수, Time: 108 ms, Memory: 111440 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Gold/33489. 수열의 점수/수열의 점수.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+for t in range(int(input())):
+    x,y = map(int,input().split())
+    if x <= 2:
+        tmp = 1
+    else:
+        tmp = x//2+1
+    if tmp > y:
+        tmp = 1
+    print(x,tmp)
