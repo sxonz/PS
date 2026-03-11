2c6cc3b1a73d3afd736535e4b85ad4b7a45fa873
sxonz <taebal2020@gmail.com>
2026-03-11 14:03:22 +0900

    [Platinum IV] Title: 트리, Time: 416 ms, Memory: 67436 KB, Score: 43 point -BaekjoonHub

---
+++ 백준/Platinum/13306. 트리/트리.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+n,q = map(int,input().split())
+tmp = [0,0]+[int(input()) for i in range(n-1)]
+par = list(range(n+1))
+def F(x):
+    if x != par[x]:
+        par[x] = F(par[x])
+    return par[x]
+def U(a,b):
+    a,b = F(a),F(b)
+    if a<b:
+        par[b] = a
+    else:
+        par[a] = b
+
+query = [tuple(map(int,input().split())) for i in range(n-1+q)][::-1]
+ans = []
+for Q in query:
+    q,*r = Q
+    if q == 0:
+        U(r[0],tmp[r[0]])
+    else:
+        if F(r[0]) == F(r[1]):
+            ans.append("YES")
+        else:
+            ans.append("NO")
+print(*ans[::-1],sep='\n')
