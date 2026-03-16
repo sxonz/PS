26d247e2cbf4a64c464ca11941a63ea419938631
sxonz <taebal2020@gmail.com>
2026-03-16 17:09:24 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 436 ms, Memory: 112920 KB, Score: 50 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -9,7 +9,7 @@ def power(x):
     elif x%2:
         return power(x//2)*power(x//2)*2%MOD
     else:
-        return power(x//2)*power(x//2)
+        return power(x//2)*power(x//2)%MOD
 for i in range(n):
     ans = (ans+power(i)*d[i])%MOD
     ans = (ans-power(n-i-1)*d[i])%MOD
