b8fab07c583b83adbcdf0f45eaa6b73c87890f21
sxonz <taebal2020@gmail.com>
2026-03-20 13:47:29 +0900

    [Silver IV] Title: 수 찾기, Time: 152 ms, Memory: 52280 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,6 +1,6 @@
-n = int(input())
-nums = set(list(map(int,input().split())))
-m = int(input())
-f = list(map(int,input().split()))
-for i in f:
-    print(int(i in nums))
+input()
+d = set(list(map(int,input().split())))
+input()
+r = list(map(int,input().split()))
+for i in r:
+    print(int(i in d))
