7f007effb03d2668abc8bf30254b9f15ba43ebc0
sxonz <taebal2020@gmail.com>
2026-03-26 02:34:30 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.py
@@ -0,0 +1,11 @@
+while True:
+    s = input()
+    if s == "end":
+        break
+    if s == "animal":
+        print("Panthera tigris")
+    elif s == "flower":
+        print("Forsythia koreana")
+    else:
+        print("Pinus densiflora")
+        
