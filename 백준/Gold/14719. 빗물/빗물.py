decb5499d05d0ab77eba3f2cdb175434f0bb7021
sxonz <taebal2020@gmail.com>
2026-04-02 17:30:10 +0900

    [Gold V] Title: 빗물, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,23 +1,14 @@
-n,m = map(int,input().split())
+h,n = map(int,input().split())
 d = list(map(int,input().split()))
-board = [[0]*n for i in range(m)]
-for i in range(m):
-    for j in range(d[i]):
-        board[i][j] = 1
-cnt = 0
-for i in range(m):
-    for j in range(n):
-        if not board[i][j]:
-            flag = False
-            for k in range(i):
-                if board[k][j]:
-                    break
-            else:
-                flag |= True
-            for k in range(i+1,m):
-                if board[k][j]:
-                    break
-            else:
-                flag |= True
-            cnt += flag^1
-print(cnt)
+ans = 0
+stack = []
+maxv = 0
+for i in range(n):
+    maxv = max(maxv,d[i])
+    tmp = 1
+    while stack and stack[-1][0] < d[i]:
+        r = stack.pop()
+        ans += r[2]*(min(d[i],r[1])-r[0])
+        tmp += r[2]
+    stack.append((d[i],maxv,tmp))
+print(ans)
