0d70e769d36516d3e24b1d910f370d063672b7cf
sxonz <taebal2020@gmail.com>
2026-04-20 11:24:13 +0900

    [Ruby V] Title: 나이트투어, Time: 1268 ms, Memory: 221100 KB, Score: 28 point -BaekjoonHub

--- 백준/Ruby/2025. 나이트투어/나이트투어.py
+++ 백준/Ruby/2025. 나이트투어/나이트투어.py
@@ -1,34 +1,40 @@
+from random import *
 dx = (-2,-2,-1,1,2,2,1,-1)
 dy = (-1,1,2,2,1,-1,-2,-2)
 n = int(input())
 x,y = map(int,input().split())
-cx,cy = x-1,y-1
-r = [(cx,cy)]
-visited = [[0]*n for i in range(n)]
-visited[cx][cy] = 1
-def check(x,y):
-    res = 0
-    for i in range(8):
-        nx,ny = cx+dx[i],cy+dy[i]
-        if 0<=nx<n and 0<=ny<n:
-            if not visited[nx][ny]:
-                res += 1
-    return res
-for c in range(n*n-1):
-    tmp = []
-    for i in range(8):
-        nx,ny = cx+dx[i],cy+dy[i]
-        if 0<=nx<n and 0<=ny<n:
-            if not visited[nx][ny]:
-                tmp.append((check(nx,ny),nx,ny))
-    if not tmp:
-        break
-    cx,cy = min(tmp)[1:]
+for i in range(100):
+    cx,cy = x-1,y-1
+    r = [(cx,cy)]
+    visited = [[0]*n for i in range(n)]
     visited[cx][cy] = 1
-    r.append((cx,cy))
+    def check(x,y):
+        res = 0
+        for i in range(8):
+            nx,ny = cx+dx[i],cy+dy[i]
+            if 0<=nx<n and 0<=ny<n:
+                if not visited[nx][ny]:
+                    res += 1
+        return res
+    for c in range(n*n-1):
+        tmp = []
+        for i in range(8):
+            nx,ny = cx+dx[i],cy+dy[i]
+            if 0<=nx<n and 0<=ny<n:
+                if not visited[nx][ny]:
+                    if tmp and check(nx,ny) < tmp[0][0]:
+                        tmp.clear()
+                        tmp.append((check(nx,ny),nx,ny))
+                    elif not tmp or check(nx,ny) == tmp[0][0]:
+                        tmp.append((check(nx,ny),nx,ny))
+        if not tmp:
+            break
+        cx,cy = tmp[randrange(len(tmp))][1:]
+        visited[cx][cy] = 1
+        r.append((cx+1,cy+1))
 
-if len(r) == n*n:
-    for i in r:
-        print(*i)
-else:
-    print(-1,-1)
+    if len(r) == n*n:
+        for i in r:
+            print(*i)
+            exit()
+print(-1,-1)
