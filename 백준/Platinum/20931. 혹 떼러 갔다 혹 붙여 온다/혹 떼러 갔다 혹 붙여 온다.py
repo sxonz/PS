e1e7fe277cff0f125aec7c75177eeda48f0acf32
sxonz <taebal2020@gmail.com>
2026-04-22 13:18:49 +0900

    [Platinum IV] Title: 혹 떼러 갔다 혹 붙여 온다, Time: 1976 ms, Memory: 249960 KB -BaekjoonHub

--- 백준/Platinum/20931. 혹 떼러 갔다 혹 붙여 온다/혹 떼러 갔다 혹 붙여 온다.py
+++ 백준/Platinum/20931. 혹 떼러 갔다 혹 붙여 온다/혹 떼러 갔다 혹 붙여 온다.py
@@ -3,15 +3,15 @@ I,R=sys.stdin.readline,range
 L,M=19,-1
 P,D,m,last=[[0]*L],[[int(2e22)]*L],1,0
 def adhoc(k,l):
-    global m;x,y=[(k+last)%m],[l]
-    for r in R(L-1):x+=[P[t:=x[M]][r]];y+=[y[M] + D[t][r]]
-    P.append(x);D.append(y);m+=1
+ global m;x,y=[(k+last)%m],[l]
+ for r in R(L-1):x+=[P[t:=x[M]][r]];y+=[y[M] + D[t][r]]
+ P.append(x);D.append(y);m+=1
 def query(x,l):
-    global last
-    for k in R(L-1,M,M):
-        if l>=(d:=D[x][k]):l-=d;x=P[x][k]
-    print(last:=x)
+ global last
+ for k in R(L-1,M,M):
+  if l>=(d:=D[x][k]):l-=d;x=P[x][k]
+ print(last:=x)
 for q in R(int(I())):
-    s,a,b = I().split();a,b=int(a),int(b)
-    if"q"==s[0]:query(a,b)
-    else:adhoc(a,b)
+ s,a,b = I().split();a,b=int(a),int(b)
+ if"q"==s[0]:query(a,b)
+ else:adhoc(a,b)
